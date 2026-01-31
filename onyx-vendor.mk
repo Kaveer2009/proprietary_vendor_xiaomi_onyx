627e8c1227a0f4d607e3b25fe37f8d5434b98fd7
AdarshGrewal <adarshgrewal@gmail.com>
2026-02-01 00:03:34 +0530

    onyx: Build AOSP NXP authsecret HAL
    Change-Id: Ib47fc9148f081501a2cdbea02de882cf4a0e2ac5

--- onyx-vendor.mk
+++ onyx-vendor.mk
@@ -1045,7 +1045,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/onyx/proprietary/odm/etc/display/qdcm_calib_data_xiaomi_o10u_36_02_0b_video_mode_dsc_dsi_panel.json:$(TARGET_COPY_OUT_ODM)/etc/display/qdcm_calib_data_xiaomi_o10u_36_02_0b_video_mode_dsc_dsi_panel.json \
     vendor/xiaomi/onyx/proprietary/odm/etc/display/qdcm_calib_data_xiaomi_o10u_42_02_0a_video_mode_dsc_dsi_panel.json:$(TARGET_COPY_OUT_ODM)/etc/display/qdcm_calib_data_xiaomi_o10u_42_02_0a_video_mode_dsc_dsi_panel.json \
     vendor/xiaomi/onyx/proprietary/odm/etc/init.panel_info.sh:$(TARGET_COPY_OUT_ODM)/etc/init.panel_info.sh \
-    vendor/xiaomi/onyx/proprietary/odm/etc/init/android.hardware.authsecret-service.nxp.rc:$(TARGET_COPY_OUT_ODM)/etc/init/android.hardware.authsecret-service.nxp.rc \
     vendor/xiaomi/onyx/proprietary/odm/etc/init/android.hardware.security.keymint-service.strongbox.nxp.rc:$(TARGET_COPY_OUT_ODM)/etc/init/android.hardware.security.keymint-service.strongbox.nxp.rc \
     vendor/xiaomi/onyx/proprietary/odm/etc/init/android.hardware.weaver-service.nxp.rc:$(TARGET_COPY_OUT_ODM)/etc/init/android.hardware.weaver-service.nxp.rc \
     vendor/xiaomi/onyx/proprietary/odm/etc/init/init.awinic.rc:$(TARGET_COPY_OUT_ODM)/etc/init/init.awinic.rc \
@@ -3293,7 +3292,6 @@ PRODUCT_PACKAGES += \
     libarcsoft_turbo_hdr_grf \
     libarcsoft_turbo_hdr_raw \
     libaudioroute_ext \
-    libauthsecret_empty-nxp \
     libawcal \
     libcamera_scene \
     libclient2slpi.notifier \
@@ -3484,7 +3482,6 @@ PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.quickcamera.xml \
     vendor.qti.qccsyshal_aidl-service.xml \
     vendor.qti.qesdsys.service.xml \
-    android.hardware.authsecret-service.nxp.xml \
     android.hardware.security.keymint-service.strongbox.xml \
     android.hardware.security.sharedsecret-service.strongbox.xml \
     android.hardware.weaver-service.nxp.xml \
@@ -3601,7 +3598,6 @@ PRODUCT_PACKAGES += \
     xtwifi-client \
     qccsyshal_aidl-service \
     wfdservice64 \
-    android.hardware.authsecret-service.nxp-qti \
     android.hardware.security.keymint-service.strongbox-nxp \
     android.hardware.weaver-service.nxp-qti \
     misensor_camera \
