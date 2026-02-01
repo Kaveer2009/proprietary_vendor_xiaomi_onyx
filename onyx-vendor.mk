10f70aa711170d71137de738a8b79db88d2e9e1d
AdarshGrewal <adarshgrewal@gmail.com>
2026-02-02 03:33:27 +0530

    onyx: Remove useage of android.media.audio.common.types-V6-ndk
    Change-Id: Iff9f39f49224e4b8b614d5cac2334f6caf43ad4a

--- onyx-vendor.mk
+++ onyx-vendor.mk
@@ -1956,7 +1956,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.diaghal-V1-ndk \
     android.hardware.bluetooth.audio-impl_prebuilt \
-    android.media.audio.common.types-V6-ndk \
     btaudio_offload_if \
     com.mi.node.fd \
     com.mi.node.miaihighlight \
