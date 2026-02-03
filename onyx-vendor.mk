0c849a63c2be1c5640d32d42b18239316fbf6da4
AdarshGrewal <adarshgrewal@gmail.com>
2026-02-03 23:44:05 +0530

    onyx: Drop extra xiaomi qcodec plugins
    Change-Id: If6de451c595c7560ac5790524ea9d73c3f35cc11

--- onyx-vendor.mk
+++ onyx-vendor.mk
@@ -2222,8 +2222,6 @@ PRODUCT_PACKAGES += \
     libVoiceSdk \
     libWlanServiceJni \
     libXMFaceHeadBodyTrack \
-    lib_AIVC_decoder \
-    lib_AIVC_encoder \
     lib_bt_lhdc \
     lib_bt_mihc \
     libadm \
@@ -2548,13 +2546,9 @@ PRODUCT_PACKAGES += \
     libqcodec2_imgtxrfilter \
     libqcodec2_mockfilter \
     libqcodec2_platform \
-    libqcodec2_psfilter \
-    libqcodec2_rffilter \
-    libqcodec2_srfilter \
     libqcodec2_streaminteg_filter \
     libqcodec2_utils \
     libqcodec2_v4l2codec \
-    libqcodec2_videofrcfilter \
     libqcrilDataModule \
     libqcrilNr \
     libqcrilNrCellInfoModule \
